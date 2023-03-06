ab7c395e6a33a3fd83519c1562b4c692c10039de
Mehran Jillani <mehran.jillani@zigron.com>
2023-03-06 17:25:24 +0500

    layouts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss'; 
-import { Layout } from './layout';
+
 
 function App() {
   return (
     <div className="App">
-      <Layout/>
+     Hello world
     </div>
   );
 }
---
+++ src/layout/Protected/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+ 
+
+export default function index() {
+  return (
+    <div>Protected Layout and routing like login etc</div>
+  )
+}
