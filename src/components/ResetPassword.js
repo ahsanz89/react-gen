ab06688f7932afdfe10fb5265e5936cea194a590
Javed <javed.iqbal@zigron.com>
2023-03-06 14:51:50 +0500

    Add ResetPassword Component

---
+++ src/components/ResetPassword.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ResetPassword(props) {
+  return (
+    <>
+      <h1>Reset Password</h1>
+      <form>
+        <input type="button" value="Hello" />{" "}
+      </form>
+    </>
+  );
+}
