94c9c817e421fa26b0f146afc5f3d0ff07ce032d
Mehran Jillani <mehran.jillani@zigron.com>
2023-03-06 15:23:14 +0500

    basic structure

--- src/components/ForgotPassword.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ForgotPassword(props) {
-  return (
-    <>
-      <H1>Hello</H1>
-    </>
-  );
-}
--- src/components/Login.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Login(props) {
-  return (
-    <>
-      <h1>Login</h1>
-    </>
-  );
-}
