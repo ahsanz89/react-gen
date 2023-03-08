4bbc046c29947528bb967164f528d13788424644
Mehran Jillani <mehran.jillani@zigron.com>
2023-03-08 12:44:22 +0500

    resolve issue

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Routes from "./routes"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Routes />
+    <Routes />
   </React.StrictMode>
 );
 
