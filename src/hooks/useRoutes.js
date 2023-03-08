3ff2e9ae54d675fea5b3714cf53fa05a96dcccb0
Mehran Jillani <mehran.jillani@zigron.com>
2023-03-08 21:11:20 +0500

    code improvement use callback hook to memorize routes

--- src/hooks/useRoutes.js
+++ src/hooks/useRoutes.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios"
 import { routes as ROUTES } from '../data/constant';
 import * as Component from '../pages';
@@ -7,44 +7,59 @@ import * as Layout from "../layout"
 const useRoutes = (returnedValue = "") => {
     const [routes, setRoutes] = useState([]);
     const [sidebarMenu, setSidebarMenu] = useState([]);
-
+    
+    // add menu and sub menu route in single array 
+    const getAllRoutes = useCallback((routes) => {
+        routes =  routes.map(route => { 
+            return [...[route], ...getChild(route?.children)]
+        })
+        return routes.flat() || []
+    }, [])
 
     useEffect(() => {
         async function fetchRoutesData() {
-            const { data } = await axios.get('/routes/user.json'); 
-            const sidebarLinks = getSideBarLinks(data[0].children || []);
-            let routes = [];
-            data.map(route=>{
-                routes.push(route)
-                routes = [...routes,...getChild(route.children || [])].flat()
-                return routes
-            }) 
+            const { data } = await axios.get('/routes/admin.json');
+            const findLayoutIndex = data.findIndex(d=>d.element === ROUTES.DEFAULT_LAYOUT_COMPONENT);
+            const sidebarLinks = getSideBarLinks(data[findLayoutIndex].children || []);
+            let routes = getAllRoutes(data); 
             routes = getRoutesComponent(routes);
-            console.log(routes)
+            
             setRoutes(routes);
             setSidebarMenu(sidebarLinks);
         }
         fetchRoutesData()
-    }, [])
- 
-    function getChild(routes){ 
-        return routes.map(route => {
-            if(route.children && route.children.length>0){
-                return route.children
-            }
-            return route
+    }, [getAllRoutes])
+
+    // Get Child component 
+    function getChild(routes) {
+        if (routes) {
+            routes = routes.map(route => {
+                if (route.children && route.children.length > 0) {
+                    return route.children
+                }
+                return [route]
+            }) 
+            return routes.flat()
+        }
+        return []
+    }
+    //  Get element JSX component value based on element name
+    const getRoutesComponent = (routes) => { 
+        return routes.map(route => { 
+            // layout and pages creation
+            const GenaricComponent = route.element === ROUTES.DEFAULT_LAYOUT_COMPONENT ? Layout[route.element] : Component[route.element]
+            route.element = <GenaricComponent />;
+            // set error component creation
+            const ErrorComponent = Component[route.errorElement] || Component[ROUTES.DEFAULT_ERROR_COMPONENT];
+            route.errorElement = <ErrorComponent />
+            return route;
         })
+
     }
 
-    const getRoutesComponent = (routes) => routes.map(route => { 
-        // layout and pages creation
-        const GenaricComponent = route.element === ROUTES.DEFAULT_LAYOUT_COMPONENT ? Layout[route.element] : Component[route.element]
-        route.element = <GenaricComponent />; 
-        // set error component
-        const ErrorComponent = Component[route.errorElement] || Component[ROUTES.DEFAULT_ERROR_COMPONENT];
-        route.errorElement = <ErrorComponent />
-        return route;
-    })
+
+
+
 
     const getSideBarLinks = (routes) => routes.map(route => {
         return {
