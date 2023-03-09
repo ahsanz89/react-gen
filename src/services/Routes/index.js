06a1eac5412fe1c671ec21c1accb5554b000f7e1
Mehran Jillani <mehran.jillani@zigron.com>
2023-03-09 12:48:47 +0500

    use axios inteceptor

--- src/services/Routes/index.js
+++ src/services/Routes/index.js
@@ -1,7 +1,7 @@
-import axios from "axios"
 import { GET_ROUTES } from "../../data/endPoints";
+import { apiRequestAxio } from "../Axios";
 
 export const getRoutes = async() => {
-   const res = await axios.get(GET_ROUTES);
+   const res = await apiRequestAxio("GET", GET_ROUTES)
    return res.data;
 }
